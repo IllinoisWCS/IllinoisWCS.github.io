d47bcf88d8cc7ffac7f5f55b28c3853ea38d2550
emilytran <emilyt@outlook.com>
2014-06-22 14:22:35 -0700

    test

--- assets/js/about_officers.js
+++ assets/js/about_officers.js
@@ -1,3 +1,29 @@
+function make_officer_section(officer)
+{
+	//replace this with a regex later, g stands for all
+	var new_id = officer.name.replace(/ /g, '');
+	new_id = new_id.replace('(', '');
+	new_id = new_id.replace(')', '');
+	
+	
+	var new_string = '<div id = "' + new_id + '" class="officer"> \
+		<div class="officer-circular"></div> \
+		<div class="officer-description"> \
+			<span class="officer-header"><span class="officer_position"><b>' + officer.position + '</b></span> | ' + officer.name + '| <a href="mailto:'+ officer.email +'">'+ officer.email +'</a> </br></span> \
+			<b>Year in School:</b> ' + officer.year + '</br> \
+			<b>Majors and Minors:</b> ' + officer.studies + '</br> \
+			<b>Where You\'re From:</b> ' + officer.from + '</br> \
+			<b>Interest in CS:</b> ' + officer.interest + '</br> \
+			<b>Previous Work Experience:</b> ' + officer.work_experience +'</br> \
+			<b>Hobbies:</b> ' + officer.hobbies + '</br> \
+			<b>Fun Fact:</b> ' + officer.fun_fact + '</br> \
+		</div> \
+	</div>';
+	
+	$("#current-officers").append(new_string);
+}
+
+
 var emily = {
 	position: "President",
 	name: "Emily Tran",
