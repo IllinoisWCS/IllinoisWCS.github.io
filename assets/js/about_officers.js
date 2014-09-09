b8aeb6289e03f8c2644a15fc4e8618ea86d01581
lilysellers <lseller2@illinois.edu>
2014-09-08 21:30:49 -0500

    Fixed space
    Fixed it so that there is a space between the officer name and the
    divider |

--- assets/js/about_officers.js
+++ assets/js/about_officers.js
@@ -9,7 +9,7 @@ function make_officer_section(officer)
 	var new_string = '<div id = "' + new_id + '" class="officer"> \
 		<div class="officer-circular"></div> \
 		<div class="officer-description"> \
-			<span class="officer-header"><span class="officer_position"><b>' + officer.position + '</b></span> | ' + officer.name + '| <a href="mailto:'+ officer.email +'">'+ officer.email +'</a> </br></span> \
+			<span class="officer-header"><span class="officer_position"><b>' + officer.position + '</b></span> | ' + officer.name +  '<b> </b>'+'| <a href="mailto:'+ officer.email +'">'+ officer.email +'</a> </br></span> \
 			<b>Year in School:</b> ' + officer.year + '</br> \
 			<b>Majors and Minors:</b> ' + officer.studies + '</br> \
 			<b>Where You\'re From:</b> ' + officer.from + '</br> \
