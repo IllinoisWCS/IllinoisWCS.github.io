f48f697fb6790fec0b460c479c084570cab3fa15
Emily Chao <elchao96@live.com>
2016-01-08 14:26:13 -0500

    Added points UI

---
+++ assets/js/members.js
@@ -0,0 +1,21 @@
+var requestURL = 'https://script.google.com/macros/s/AKfycbw4xxn9dJktu0RVJlCGQ6mIGld9BTzqc24P0bvNMWkB_SC-vmw/exec';
+
+var getPoints = function() {
+    var netid = $('#netid').val();
+    if (netid.length > 0) {
+        $.ajax({
+            url: requestURL,
+            type: 'get',
+            data: 'netid=' + encodeURIComponent(netid),
+            success: function(data) {
+                var template = Mustache.render($('#pointTemplate').html(), data);
+                $('#stats').html(template);
+            },
+            error: function(error) {
+                $('#stats').html('<p>Something went wrong. Please let us know. We are sorry for the inconvenienc.</p>');
+            }
+        });
+    } else {
+        $('#stats').html('<p>Please enter a valid netid.</p>');
+    }
+}
