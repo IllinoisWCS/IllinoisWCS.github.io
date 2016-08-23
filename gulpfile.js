73ff1d8c98101b1d8007c113d0822c877b281bae
Sujay Khandekar <khandek2@illinois.edu>
2016-08-22 23:24:54 -0500

    updated Gulp file to have live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
 	gulp.src('./assets/sass/style.scss')
 	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./assets/css'));
+	.pipe(gulp.dest('./assets/css'))
+	.pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('default', ['sass']);
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: '.'
+    },
+  });
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+	gulp.watch('./assets/sass/style.scss', ['sass']);
+});
+
+
+gulp.task('default', ['watch']);
