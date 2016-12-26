8d70afb9be8358ec65cc7c582375cab0a58b1ff5
Surabhi Sonali <ssonali2@illinois.edu>
2016-12-26 10:12:03 -0800

    moved all large JS objects into json files

--- assets/js/alumni.js
+++ assets/js/alumni.js
@@ -9,91 +9,18 @@ $(document).ready(function() {
 		}
 	};
 
-	var featuredAlumni = {
-		"featured": [
-			{
-				"name": "Julia Syi",
-				"degree": "Bachelor's in Computer Science",
-				"grad": "May 2014",
-				"involvement": "2010-2011 Member, 2012-2014 Secretary",
-				"where": "Program Manager at Microsoft",
-				"img": "assets/img/alumni/juliasyi.jpg",
-				"i": 0
-			},
-			{
-				"name": "Lavanya Iyer",
-				"degree": "Bachelor's in Computer Science",
-				"grad": "May 2014",
-				"involvement": "2012-2014 President",
-				"where": "Technology Analyst at Goldman Sachs",
-				"img": "assets/img/alumni/lavanyaiyer.jpg",
-				"i": 1
-			},
-			{
-				"name": "Kamilah Taylor",
-				"degree": "Master's in Computer Science",
-				"grad": "2010",
-				"involvement": "2007-2008 member, 2008-2010 Grad Representative",
-				"where": "Software Engineer at LinkedIn",
-				"img": "assets/img/alumni/kamilahtaylor.jpg",
-				"i": 2
-			},
-			{
-				"name": "Emily Tran",
-				"degree": "Bachelor's in Computer Science",
-				"grad": "May 2015",
-				"involvement": "2014-2015 President, 2013-2014 Vice President",
-				"where": "Program Manager at Microsoft",
-				"img": "assets/img/alumni/emilytran.jpg",
-				"i": 3
-			},
-			{
-				"name": "Deepti Gupta",
-				"degree": "Bachelor's in Computer Science",
-				"grad": "May 2015",
-				"involvement": "2014-2015 Treasurer",
-				"where": "Software Engineer at PayPal",
-				"img": "assets/img/alumni/deeptigupta.jpg",
-				"i": 4
-			},
-			{
-				"name": "Anne Dailidonis",
-				"degree": "Bachelor's in Computer Science",
-				"grad": "May 2014",
-				"involvement": "2013-2014 Mentoring Director",
-				"where": "Analyst at JP Morgan Chase",
-				"img": "assets/img/alumni/annedailidonis.jpeg",
-				"i": 5
-			},
-			{
-				"name": "Gergana Slavova",
-				"degree": "Bachelor's in Computer Science",
-				"grad": "May 2005",
-				"involvement": "Mentoring Chair, Tech Team Chair, Vice President, Co-President",
-				"where": "Software Engineer at Intel Corporation in Champaign, IL",
-				"img": "assets/img/alumni/gerganaslavova.jpg",
-				"i": 6
-			},
-			{
-				"name": "Fernanda (Mendes) Richnak",
-				"degree": "Bachelor's in Computer Science",
-				"grad": "May 2010",
-				"involvement": "2009-2010 President",
-				"where": "Program Manager at Microsoft",
-				"img": "assets/img/alumni/fernandarichnak.png",
-				"i": 7
-			},
-			{
-				"name": "Yalan (Maya) Meng",
-				"degree": "Bachelor's in Computer Science",
-				"grad": "May 2015",
-				"involvement": "2013-2014 Tech Team Director",
-				"where": "Software Engineer at Google San Francisco",
-				"img": "assets/img/alumni/mayameng.jpg",
-				"i": 8
-			}
-		]
-	};
+	var featuredAlumni = $.getJSON("alumni.json", function() {
+  console.log( "success" );
+ })
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
 
 	//shuffle(featuredAlumni.featured);
 
--- assets/js/chictech.js
+++ assets/js/chictech.js
@@ -7,150 +7,47 @@ $(document).ready(function() {
         event.preventDefault();
     });
 
-    var faq = {
-        "questions": [
-            {
-                "id": "where",
-                "question": "Where will the retreat be held?",
-                "answer": "The retreat will be held at the Thomas M. Siebel Center for Computer Science, at 201 N. Goodwin Ave., Urbana, IL 61801. This is the main building where our computer science courses are held."
-            },
-            {
-                "id": "when",
-                "question": "When is it?",
-                "answer": "Chictech will be held Nov. 12 - 13, 2016."
-            },
-            {
-                "id": "cost",
-                "question": "Are there costs associated with attending the retreat?",
-                "answer": "Nope! Once you've arrived at our retreat, it's absolutely free!"
-            },
-            {
-                "id": "registration",
-                "question": "When will registration open?",
-                "answer": "Registration will open on October 3rd and will close on October 14th. The form to register on will be in the register section below."
-
-            },
-            {
-                "id": "accept",
-                "question": "How do I know if I'm accepted into the retreat?",
-                "answer": "We will be sending out emails by October 21st with decisions about your acceptance. The spots will mostly be first come, first serve with some emphasis on the brief explanation about why you would like to attend our retreat."
-            },
-            {
-                "id": "programming",
-                "question": "Is any prior programming experience required?",
-                "answer": "No, you do not have to have programming experience prior to attending our retreat. We will provide three different workshop track options, and there is an option that requires no previous experience. Just please make sure to mark this on the registration form."
-            },
-            {
-                "id": "pack",
-                "question": "What should I pack?",
-                "answer": "Since we are an overnight retreat, a sleeping bag, pillow, clothes and pajamas, and necessary toiletries should be sufficient. If you have a sleeping mat that you would like to use, that would be great to bring as well!"
-            },
-            {
-                "id": "parking",
-                "heading": "parkingH",
-                "question": "I am going to be driving to the retreat. Where can I park?",
-                "answer": "There will be places where you can park for free! Parking is available directly south and northeast of Siebel Center in Lot B2 (on Springfield Avenue between Mathews & Goodwin) and Lot B18 (Harvey Street between Clark and Main), respectively. For Lot B2, go half a block farther from Siebel Center and enter on your right. For Lot B18, from University Ave make a left turn onto Harvey St. Go about a block and a half. The parking lot will be on your right."
-            },
-            {
-                "id": "middle",
-                "question": "I have a middle schooler who wants to attend. Can she?",
-                "answer": "We are first opening our retreat to female high school students, but we may accept middle schoolers if there are still spots left. If you have a middle schooler who wants to attend, go ahead and write that in on the registration form where it asks for the year in school. We will notify you by October 24th about whether or not you were accepted!"
-            }
-        ]
-    };
+    var faq = $.getJSON("chictech_questions.json", function() {
+      console.log("success");
+    })
+      .done(function() {
+        console.log("second success");
+    })
+      .fail(function() {
+        console.log("error");
+    })
+      .always(function() {
+        console.log("complete");
+  });
 
     var faqTemplate = Mustache.render($('#faqTemplate').html(), faq);
     $('#accordion').html(faqTemplate);
 
-    var schedule117 = {
-        "events": [
-            {
-                "time": "8:30 AM",
-                "what": "Check-in & Breakfast & Photo booth",
-                "where": "SC Atrium"
-            },
-            {
-                "time": "9:30 AM",
-                "what": "Welcome & Lean In Talk & Project Fair",
-                "where": "SC 2405"
-            },
-            {
-                "time": "10:30 AM",
-                "what": "Workshop Intros & time to get started on projects",
-                "where": "SC 0218/20/22"
-            },
-            {
-                "time": "12:00 PM",
-                "what": "Lunch & Talk from a developer in the industry",
-                "where": "SC 2405"
-            },
-            {
-                "time": "1:00 PM",
-                "what": "Workshop time to work on projects",
-                "where": "SC 0218/20/22"
-            },
-            {
-                "time": "4:00 PM",
-                "what": "Photo Scavenger Hunt",
-                "where": "Engineering Quad"
-            },
-            {
-                "time": "5:30 PM",
-                "what": "Jeopardy",
-                "where": "SC 2405"
-            },
-            {
-                "time": "6:00 PM",
-                "what": "Dinner & talk by a professor",
-                "where": "SC 2405"
-            },
-            {
-                "time": "7:00 PM",
-                "what": "Team building!",
-                "where": "SC 2405 or 1404"
-            },
-            {
-                "time": "8:00 PM",
-                "what": "Girls Night (Movie, Nail Painting, Jewelry)",
-                "where": "SC 1404"
-            }
-        ]
-    };
+    var schedule117 = $.getJSON("chictech_schedule117.json",        function() {
+      console.log("success");
+    })
+      .done(function() {
+        console.log("second success");
+    })
+      .fail(function() {
+        console.log("error");
+    })
+      .always(function() {
+        console.log("complete");
+  });
 
-    var schedule118 = {
-        "events": [
-            {
-                "time": "9:00 AM",
-                "what": "Breakfast",
-                "where": "SC 2405"
-            },
-            {
-                "time": "9:30 AM",
-                "what": "Finish Breakfast during Panel & Photo",
-                "where": "SC 2405 or 1404"
-            },
-            {
-                "time": "10:30 AM",
-                "what": "Workshop time to work on projects",
-                "where": "SC 0218/20/22"
-            },
-            {
-                "time": "12:00 PM",
-                "what": "Start project demos to parents",
-                "where": "SC 0218/20/22"
-            },
-            {
-                "time": "12:30 PM",
-                "what": "Lunch (parents invited) and talk by current professor",
-                "where": "SC 1404"
-            },
-            {
-                "time": "1:30 PM",
-                "what": "Wrap up and Closing Note",
-                "where": "SC 2405"
-            }
-        ]
-    };
+    var schedule118 = $.getJSON("chictech_schedule118.json",         function() {
+      console.log("success");
+    })
+      .done(function() {
+        console.log("second success");
+    })
+      .fail(function() {
+        console.log("error");
+    })
+      .always(function() {
+        console.log("complete");
+  });
 
     var schedule117Template = Mustache.render($('#eventTemplate').html(), schedule117);
     $('#agenda117').html(schedule117Template);
