54d6d52437e7059e9f65ba2d67a1dc72075c19d0
Sujay Khandekar <khandek2@illinois.edu>
2017-02-10 03:01:00 -0600

    fixed the modal issue

--- assets/js/officers.js
+++ assets/js/officers.js
@@ -1,15 +1,6 @@
 $(document).ready(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+    $.getJSON("../assets/js/officers.json", function(data) {
+        var output = Mustache.render($('#officerTemplate').html(), data);
+        $('#allOfficers').html(output);
     });
-
-  $.getJSON("../assets/js/officers.json", function(data) {
-    var output = Mustache.render($('#officerTemplate').html(), data);
-    $('#allOfficers').html(output);
-  });
-
 });
