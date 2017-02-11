99b38b86572feb40c2b6f1a81ef302ab9e32779a
Sujay Khandekar <khandek2@illinois.edu>
2017-02-10 23:30:37 -0600

    adding in the navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -133,12 +133,3 @@ $(document).ready(function() {
     }
 
 });
-
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
