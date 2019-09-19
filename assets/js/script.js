cb97bcca12ddcc225f21d86857ed858caf0464a8
annabuyevich <avbuyevich@gmail.com>
2019-09-19 17:10:47 -0500

    added medium article, updated the navbar, updated upcoming events for home page and tech team

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,19 @@ $(document).ready(function() {
     var events = [];
     $.get(eventsUrl, function(data) {
         events = data.slice(0, 5);
+        console.log(events);
         $.each(events, function(i) {
-
+            // console.log(event[i]);
             var start = new Date(events[i].startTime);
+
             var li = $('<li/>')
                 .addClass('list-group-item');
             li.append('<h6>' + events[i].title + '</h6>');
-            li.append('<span>' + start.toDateString() + ' at ' + start.toLocaleTimeString() + '</span>');
+            li.append('<span>' + start.toDateString() + ' at ' + start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '</span>');
+            if (events[i].location.length > 0) {
+                li.append('<br><span>' + ' Location: ' + events[i].location + '</span>');
+            }
+
             $('#events-list').append(li);
         });
     });
@@ -36,96 +42,94 @@ $(document).ready(function() {
     var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
     /**
-    * Check if current user has authorized this application.
-    */
+     * Check if current user has authorized this application.
+     */
     function checkAuth() {
-        gapi.auth.authorize(
-          {
+        gapi.auth.authorize({
             'client_id': CLIENT_ID,
             'scope': SCOPES.join(' '),
             'immediate': true
-          }, handleAuthResult);
+        }, handleAuthResult);
     }
 
     /**
-    * Handle response from authorization server.
-    *
-    * @param {Object} authResult Authorization result.
-    */
+     * Handle response from authorization server.
+     *
+     * @param {Object} authResult Authorization result.
+     */
     function handleAuthResult(authResult) {
-    var authorizeDiv = document.getElementById('authorize-div');
-    if (authResult && !authResult.error) {
-      // Hide auth UI, then load client library.
-      authorizeDiv.style.display = 'none';
-      loadCalendarApi();
-    } else {
-      // Show auth UI, allowing the user to initiate authorization by
-      // clicking authorize button.
-      authorizeDiv.style.display = 'inline';
-    }
-    }
-
-    /**
-    * Initiate auth flow in response to user clicking authorize button.
-    *
-    * @param {Event} event Button click event.
-    */
-    function handleAuthClick(event) {
-    gapi.auth.authorize(
-      {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-      handleAuthResult);
-    return false;
-    }
-
-    /**
-    * Load Google Calendar client library. List upcoming events
-    * once client library is loaded.
-    */
-    function loadCalendarApi() {
-    gapi.client.load('calendar', 'v3', listUpcomingEvents);
-    }
-
-    /**
-    * Print the summary and start datetime/date of the next ten events in
-    * the authorized user's calendar. If no events are found an
-    * appropriate message is printed.
-    */
-    function listUpcomingEvents() {
-    var request = gapi.client.calendar.events.list({
-      'calendarId': 'illinoiswcs@gmail.com',
-      'timeMin': (new Date()).toISOString(),
-      'showDeleted': false,
-      'singleEvents': true,
-      'maxResults': 10,
-      'orderBy': 'startTime'
-    });
-
-    request.execute(function(resp) {
-      var events = resp.items;
-      appendPre('Upcoming events:');
-
-      if (events.length > 0) {
-        for (i = 0; i < events.length; i++) {
-          var event = events[i];
-          var when = event.start.dateTime;
-          if (!when) {
-            when = event.start.date;
-          }
-          appendPre(event.summary + ' (' + when + ')')
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+            // Hide auth UI, then load client library.
+            authorizeDiv.style.display = 'none';
+            loadCalendarApi();
+        } else {
+            // Show auth UI, allowing the user to initiate authorization by
+            // clicking authorize button.
+            authorizeDiv.style.display = 'inline';
         }
-      } else {
-        appendPre('No upcoming events found.');
-      }
-
-    });
     }
 
     /**
-    * Append a pre element to the body containing the given message
-    * as its text node.
-    *
-    * @param {string} message Text to be placed in pre element.
-    */
+     * Initiate auth flow in response to user clicking authorize button.
+     *
+     * @param {Event} event Button click event.
+     */
+    function handleAuthClick(event) {
+        gapi.auth.authorize({ client_id: CLIENT_ID, scope: SCOPES, immediate: false },
+            handleAuthResult);
+        return false;
+    }
+
+    /**
+     * Load Google Calendar client library. List upcoming events
+     * once client library is loaded.
+     */
+    function loadCalendarApi() {
+        gapi.client.load('calendar', 'v3', listUpcomingEvents);
+    }
+
+    /**
+     * Print the summary and start datetime/date of the next ten events in
+     * the authorized user's calendar. If no events are found an
+     * appropriate message is printed.
+     */
+    function listUpcomingEvents() {
+        var request = gapi.client.calendar.events.list({
+            'calendarId': '3c28gpprcnos0q251odnl877mc@group.calendar.google.com',
+            'timeMin': (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime'
+        });
+
+        request.execute(function(resp) {
+            var events = resp.items;
+            appendPre('Upcoming events:');
+
+            if (events.length > 0) {
+                for (i = 0; i < events.length; i++) {
+                    var event = events[i];
+                    var when = event.start.dateTime;
+                    if (!when) {
+                        when = event.start.date;
+                    }
+                    appendPre(event.summary + ' (' + when + ')')
+                }
+            } else {
+                appendPre('No upcoming events found.');
+            }
+
+        });
+    }
+
+    /**
+     * Append a pre element to the body containing the given message
+     * as its text node.
+     *
+     * @param {string} message Text to be placed in pre element.
+     */
     function appendPre(message) {
         var pre = document.getElementById('output');
         var textContent = document.createTextNode(message + '\n');
--- assets/js/techteam.js
+++ assets/js/techteam.js
@@ -17,9 +17,131 @@ $(document).ready(function() {
         $('#linksList').html(linksTemplate);
     });
 
-    $.getJSON("../assets/js/techteam_upcoming.json", function(data) {
-        var upcomingTemplate = Mustache.render($('#upcomingEvents').html(), data);
-        $('#upcomingTT').html(upcomingTemplate);
+    // $.getJSON("../assets/js/techteam_upcoming.json", function(data) {
+    //     var upcomingTemplate = Mustache.render($('#upcomingEvents').html(), data);
+    //     $('#upcomingTT').html(upcomingTemplate);
+    // });
+
+    // <tr>
+    //                 <th scope="row" style="font-weight:400">{{event}}</th>
+    //                 <td>{{date}}</td>
+    //                 <td>{{time}}</td>
+    //             </tr>
+
+    var eventsUrl = 'https://script.google.com/macros/s/AKfycbysZFMo4SZfZCtl9swTqD3M1PvyhLBD71qcrHheuoziX9FpJtpZ/exec';
+    var events = [];
+    $.get(eventsUrl, function(data) {
+        events = data;
+        console.log(events);
+        $.each(events, function(i) {
+
+
+            var tr = $('<tr/>');
+            // .addClass('list-group-item');
+            let creator = events[i].creator;
+
+            var added = false;
+            console.log("creator: ", creator);
+            if (creator.includes("mihikaa2@illinois.edu") || creator.includes("alicesf2@illinois.edu") || creator.includes("annavb2@illinois.edu")) {
+                console.log("inside");
+                let title = events[i].title;
+                tr.append('<th scope="row" style="font-weight:400">' + title + '</th>');
+
+                var start = new Date(events[i].startTime);
+                var end = new Date(events[i].endTime);
+                tr.append('<td>' + start.toDateString() + '</td>');
+                tr.append('<td>' + start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + ' - ' + end.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '</td>');
+
+                added = true;
+
+
+            }
+
+            if (added) {
+                $('#upcomingTT').append(tr);
+            }
+
+
+
+        });
     });
+
+
+    // Your Client ID can be retrieved from your project in the Google
+    // Developer Console, https://console.developers.google.com
+    var CLIENT_ID = '554068638959-nlr847cg1d1b3a08i3sjq2s123rbnn3k.apps.googleusercontent.com';
+
+    var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+    /**
+     * Check if current user has authorized this application.
+     */
+    function checkAuth() {
+        gapi.auth.authorize({
+            'client_id': CLIENT_ID,
+            'scope': SCOPES.join(' '),
+            'immediate': true
+        }, handleAuthResult);
+    }
+
+    /**
+     * Handle response from authorization server.
+     *
+     * @param {Object} authResult Authorization result.
+     */
+    function handleAuthResult(authResult) {
+        var authorizeDiv = document.getElementById('authorize-div');
+        if (authResult && !authResult.error) {
+            // Hide auth UI, then load client library.
+            authorizeDiv.style.display = 'none';
+            loadCalendarApi();
+        } else {
+            // Show auth UI, allowing the user to initiate authorization by
+            // clicking authorize button.
+            authorizeDiv.style.display = 'inline';
+        }
+    }
+    // loadCalendarApi();
+
+    function loadCalendarApi() {
+        gapi.client.load('calendar', 'v3', listUpcomingEvents);
+    }
+
+    /**
+     * Print the summary and start datetime/date of the next ten events in
+     * the authorized user's calendar. If no events are found an
+     * appropriate message is printed.
+     */
+    function listUpcomingEvents() {
+        var request = gapi.client.calendar.events.list({
+            'calendarId': '3c28gpprcnos0q251odnl877mc@group.calendar.google.com',
+            'timeMin': (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime'
+        });
+
+        request.execute(function(resp) {
+            var events = resp.items;
+            appendPre('Upcoming events:');
+
+            if (events.length > 0) {
+                for (i = 0; i < events.length; i++) {
+                    var event = events[i];
+                    var when = event.start.dateTime;
+                    if (!when) {
+                        when = event.start.date;
+                    }
+                    appendPre(event.summary + ' (' + when + ')')
+                }
+            } else {
+                appendPre('No upcoming events found.');
+            }
+
+        });
+    }
+
+
 
 });
