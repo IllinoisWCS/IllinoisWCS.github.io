b566ac718e7eeae2c2b8ff35495a70af0e1ae93e
Daniel Wang <hello@danielwang.dev>
2022-11-06 17:49:47 -0600

    Remove alumni page

--- assets/js/alumni.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $.getJSON("../assets/js/alumni.json", function(data) {
-        var template = Mustache.render($('#alumniTemplate').html(), data);
-        $('#featuredAlumni').html(template);
-    });
-});
