0ad8416d6cc58c4ccae9381a30666d719f8f794b
Alice Li <82292202+aliceli465@users.noreply.github.com>
2022-11-30 15:23:03 -0600

    Edit mustache template and .js file

--- assets/js/main_carousel.js
+++ assets/js/main_carousel.js
@@ -1,17 +1,18 @@
-function insertAfter(referenceNode, newNode) {
-    referenceNode.parentNode.insertBefore(newNode,referenceNode.nextSibling);
-}
 $(document).ready(function() {
     $.getJSON("../assets/js/main_carousel.json", function(data) {
-        const currentActive = document.getElementById("currentActive");
-        $.each(data,function(i,field) {
-            var output = Mustache.render($('#mainCarouselTemplate').html(),field);
-            insertAfter(currentActive,output);
-        });
-        // for(let x in data.main-carousel) {
-        //     var output = Mustache.render($('#mainCarouselTemplate').html(),x);
-        //     insertAfter(currentActive,output);
-        // }
-        //$('#putCarouselImages').html(output);
+        var active = document.getElementById("currentActive");
+        var size = Object.keys(data.values).length;
+        var count = 0;
+        //create new div for each image
+        for(let i = 0; i < size; i++) {
+            count+=1;
+            var output = Mustache.render($('#mainCarouselTemplate').html(), data.values[i]);
+            var ele = document.createElement("div");
+            ele.className = "item";
+            ele.innerHTML = output;
+            active.parentNode.insertBefore(ele,active.nextSibling);
+            //add indicator
+            $("#indicators").append('<li data-target="#mainCarousel" data-slide-to='+count.toString()+'></li>');
+        }
     });
 });
