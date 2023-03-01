55a1fb0273c3d42c021d561160e24faa32611c45
Harini Arumugam <3harinia@gmail.com>
2023-03-01 10:31:09 -0600

    finished committees page

---
+++ assets/js/committees.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $.getJSON("../assets/js/committees.json", function(data) {
+        var output = Mustache.render($('#committeesTemplate').html(), data);
+        $('#allCommittees').html(output);
+    });
+    
+});
