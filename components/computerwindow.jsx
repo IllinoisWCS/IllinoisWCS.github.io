81761cf51fea5182f35bc1637f46a29d2edb078b
Jacob Edley <jac.dan@icloud.com>
2023-10-18 00:49:02 -0500

    fix indenting again

--- components/computerwindow.jsx
+++ components/computerwindow.jsx
@@ -1,56 +1,56 @@
 export default function ComputerWindow({ width, height, color }) {
-  return (
-	<div>
-		<div className="container">
-			<div className="topbar">
-				<ul>
-					<li className="topbar-red-button"></li>
-					<li className="topbar-yellow-button"></li>
-					<li className="topbar-green-button"></li>
-				</ul>
+	return (
+		<div>
+			<div className="container">
+				<div className="topbar">
+					<ul>
+						<li className="topbar-red-button"></li>
+						<li className="topbar-yellow-button"></li>
+						<li className="topbar-green-button"></li>
+					</ul>
+				</div>
 			</div>
-		</div>
-		<style jsx>{`
-			.container {
-				background-color: white;
-				border-radius: 1rem;
-				display: flex;
-				height: ${height};
-				overflow: hidden;
-				width: ${width};
-			}
-	
-			.topbar {
-				background-color: ${color};
-				height: 2rem;
-				width: 100%;
-				position: relative;
-			}
-	
-			.topbar ul li {
-				border-radius: 100%;
-				height: 0.5rem;
-				list-style: none;
-				position: absolute;
-				top: 0.75rem;
-				width: 0.5rem;
-			}
-	
-			.topbar-red-button {
-				background-color: red;
-				left: 0.5rem;
-			}
-	
-			.topbar-yellow-button {
-				background-color: yellow;
-				left: 1.5rem;
-			}
-	
-			.topbar-green-button {
-				background-color: lime;
-				left: 2.5rem;
-			}
-		`}</style>
+			<style jsx>{`
+				.container {
+					background-color: white;
+					border-radius: 1rem;
+					display: flex;
+					height: ${height};
+					overflow: hidden;
+					width: ${width};
+				}
+		
+				.topbar {
+					background-color: ${color};
+					height: 2rem;
+					width: 100%;
+					position: relative;
+				}
+		
+				.topbar ul li {
+					border-radius: 100%;
+					height: 0.5rem;
+					list-style: none;
+					position: absolute;
+					top: 0.75rem;
+					width: 0.5rem;
+				}
+		
+				.topbar-red-button {
+					background-color: red;
+					left: 0.5rem;
+				}
+		
+				.topbar-yellow-button {
+					background-color: yellow;
+					left: 1.5rem;
+				}
+		
+				.topbar-green-button {
+					background-color: lime;
+					left: 2.5rem;
+				}
+			`}</style>
 		</div>
 	);
 }
