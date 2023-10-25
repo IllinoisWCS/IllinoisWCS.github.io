abc8a6f28d8ac5b140ca9df325cf35d58aa56ccb
Firmiana Wang <firmianaw@gmail.com>
2023-10-24 22:03:30 -0500

    added phone component

---
+++ src/pages/PhoneComponent.js
@@ -0,0 +1,39 @@
+const styles = {
+  container: {
+    backgroundColor: 'white',
+    width: '447px', 
+    height: '778px',
+    borderRadius: '33px',
+    border: '13px solid #BAA7FF',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center'
+  },
+  notch: {
+    width: '117px',
+    height: '33px',
+    borderRadius: '63px',
+    backgroundColor: '#BAA7FF',
+    marginTop: '12px'
+  },
+  rectangle: {
+    width: '211px',
+    height: '9px',
+    borderRadius: '63px',
+    backgroundColor: '#BAA7FF',
+    marginBottom: '12px'
+  }
+}
+export default function PhoneComponent({children}) {
+  return (
+    <div className="container" style={Object.assign(styles.container, {backgroundImage: `url(${children})`})}>
+        <div className="notch" style={styles.notch}></div>
+        <div className="rectangle" style={styles.rectangle}></div>
+    </div>
+  );
+};
+
