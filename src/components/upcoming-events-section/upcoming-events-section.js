bd3f9298ca78a8f81662bd78446db1bc6896d884
Jacob Edley <jac.dan@icloud.com>
2023-10-29 17:04:58 -0500

    style header

--- src/components/upcoming-events-section/upcoming-events-section.js
+++ src/components/upcoming-events-section/upcoming-events-section.js
@@ -6,10 +6,10 @@ export default function UpcomingEventsSection() {
   return (
     <>
       <div className={styles.container}>
-        <h1>Upcoming Events</h1>
+        <h2 className={styles.header}>Upcoming Events</h2>
         <div className={styles.eventContainer}>
           <EventsWindow location={"Siebel CS 0211"} color={"#FB79C3"}>
-            <p className={styles.officeHours} style={{color: "black"}}>Come to our office to chat, ask questions, or just study:</p>
+            <p className={styles.officeHours}>Come to our office to chat, ask questions, or just study:</p>
           </EventsWindow>
           <UpcomingEvent title={"ChicTech"} date={"Oct 07"} time={"8:00 AM - 4:00 PM"} location={"Siebel 1404 & 2405"}/>
           <UpcomingEvent title={"ChicTech"} date={"Oct 07"} time={"8:00 AM - 4:00 PM"} location={"Siebel 1404 & 2405"}/>
