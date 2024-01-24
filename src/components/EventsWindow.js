f4a088cbdfc3f8a4184aa9d66bf7f9fca039633d
neha-vard <neha.vard@gmail.com>
2024-01-24 16:10:48 -0600

    Remove description field for events

--- src/components/EventsWindow.js
+++ src/components/EventsWindow.js
@@ -1,15 +1,25 @@
 import ComputerWindow from "./ComputerWindow";
 import styles from "@/styles/EventsWindow.module.css";
 
-export default function EventsWindow({ children, topbarColor = "#65C7CC", buttonColor = "#CBEDFF", location }) {
+export default function EventsWindow({
+  children,
+  topbarColor = "#65C7CC",
+  buttonColor = "#CBEDFF",
+  location,
+}) {
   return (
     <>
       <ComputerWindow topbarColor={topbarColor}>
         <div className={styles.container}>
           <div>{children}</div>
-          <div>
-            <div className={styles.eventButton} style={{backgroundColor: buttonColor}}><p>{location}</p></div>
-          </div>
+          {location && location.length && (
+            <div
+              className={styles.eventButton}
+              style={{backgroundColor: buttonColor}}
+            >
+              <p>{location}</p>
+            </div>
+          )}
         </div>
       </ComputerWindow>
     </>
