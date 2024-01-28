a56ad215a3cecb7bcb8f24f638b3ce8831c7e3bc
Jacob Edley <jac.dan@icloud.com>
2024-01-27 22:25:54 -0600

    add links

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,9 +1,11 @@
 import styles from "@/styles/Key.module.css";
 
-export default function Key({children, bold=false}) {
+export default function Key({children, url, bold=false}) {
+  const link = url ? () => { window.location.href = url } : null
+  
   return (
     <div className={styles.grid}>
-      <div className={`${styles.keyLeft} ${bold ? styles.boldLeft : ''}`}>
+      <div onClick={link} className={`${styles.keyLeft} ${bold ? styles.boldLeft : ''}`}>
         <div className={`${styles.keyRight} ${bold ? styles.boldRight : ''}`}>
           <div className={`${styles.keyBody} ${bold ? styles.boldBody : ''}`}>
             <h3 className={styles.content}>{children}</h3>
--- src/pages/index.js
+++ src/pages/index.js
@@ -101,10 +101,10 @@ export default function Home({events}) {
         <div className={styles.stayInTouchSection}>
           <h2 className={styles.sectionHeader}>Stay in Touch▮</h2>
           <div className={styles.communicationLinks}>
-            <div className={styles.linkOne}><Key bold>Slack</Key></div>
-            <div className={styles.linkTwo}><Key>Instagram</Key></div>
-            <div className={styles.linkThree}><Key>Mailing List</Key></div>
-            <div className={styles.linkFour}><Key>Medium</Key></div>
+            <div className={styles.linkOne}><Key bold url={"https://join.slack.com/t/wcs-uiuc/shared_invite/zt-1tut8j6pi-51ilAwG8CNmYNolsQP25ew"}>Slack</Key></div>
+            <div className={styles.linkTwo}><Key url="http://www.instagram.com/illinoiswcs">Instagram</Key></div>
+            <div className={styles.linkThree}><Key url="http://illinoiswcs.us8.list-manage.com/subscribe?u=6f053b4b1cec0f29449538eff&id=65a43965f5">Mailing List</Key></div>
+            <div className={styles.linkFour}><Key url="https://medium.com/@illinoiswcs">Medium</Key></div>
           </div>
         </div>
       </main>
