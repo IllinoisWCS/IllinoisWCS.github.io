7af9b9135eb3397d707a2856d226abedee8aa206
Jacob Edley <jac.dan@icloud.com>
2024-01-30 18:09:42 -0600

    add hover effect

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,13 +1,13 @@
 import styles from "@/styles/Key.module.css";
 
-export default function Key({children, url, bold=false}) {
-  const link = url ? () => { window.location.href = url } : null
+export default function Key({children, url}) {
+  const link = url ? () => { window.open(url, "_blank").focus() } : null
   
   return (
     <div className={styles.grid}>
-      <div onClick={link} className={`${styles.keyLeft} ${bold ? styles.boldLeft : ''}`}>
-        <div className={`${styles.keyRight} ${bold ? styles.boldRight : ''}`}>
-          <div className={`${styles.keyBody} ${bold ? styles.boldBody : ''}`}>
+      <div onClick={link} className={styles.keyLeft}>
+        <div className={styles.keyRight}>
+          <div className={styles.keyBody}>
             <h3 className={styles.content}>{children}</h3>
           </div>
         </div>
--- src/components/StayInTouchSection.js
+++ src/components/StayInTouchSection.js
@@ -6,7 +6,7 @@ const StayInTouchSection = () => {
     <div className={styles.stayInTouchSection}>
       <h2 className={styles.header}>Stay in Touch</h2>
       <div className={styles.communicationLinks}>
-        <div className={styles.linkOne}><Key bold url={"https://join.slack.com/t/wcs-uiuc/shared_invite/zt-1tut8j6pi-51ilAwG8CNmYNolsQP25ew"}>Slack</Key></div>
+        <div className={styles.linkOne}><Key url={"https://join.slack.com/t/wcs-uiuc/shared_invite/zt-1tut8j6pi-51ilAwG8CNmYNolsQP25ew"}>Slack</Key></div>
         <div className={styles.linkTwo}><Key url="http://www.instagram.com/illinoiswcs">Instagram</Key></div>
         <div className={styles.linkThree}><Key url="http://illinoiswcs.us8.list-manage.com/subscribe?u=6f053b4b1cec0f29449538eff&id=65a43965f5">Mailing List</Key></div>
         <div className={styles.linkFour}><Key url="https://medium.com/@illinoiswcs">Medium</Key></div>
