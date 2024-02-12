55a467371016867c5e18e4a9e61c24fb81d1a22b
Firmiana Wang <firmianaw@gmail.com>
2024-02-12 16:42:45 -0600

    added space between committees

--- src/components/CommitteesSection.js
+++ src/components/CommitteesSection.js
@@ -1,14 +1,16 @@
 import styles from "@/styles/CommitteesSection.module.css";
 import ComputerWindow from "./ComputerWindow";
 
-export default function CommitteesSection({isLeft, isRight, name, description, officers, color = "#65C7CC", bColor = "#CBEDFF"}) {
+export default function CommitteesSection({isLeft, isRight, name, description, officers, color = "#65C7CC", bColor = "#CBEDFF", img}) {
     return (
         <div className={`${styles.container}`}>
             {isLeft && 
                 <>
                     <h2 className={`${styles.nameLeft}`}>{name}</h2> 
                     <div className={`${styles.content}`}>
-                        <ComputerWindow className={`${styles.window}`} topbarColor={color}></ComputerWindow>
+                        <ComputerWindow className={`${styles.window}`} topbarColor={color}>
+                            <img src={img}></img>
+                        </ComputerWindow>
                         <p className={`${styles.descriptionRight}`}>{description}</p>
                     </div>
                     <p className={`${styles.officersRight}`} style={{backgroundColor: bColor}}>{officers}</p>
@@ -19,7 +21,9 @@ export default function CommitteesSection({isLeft, isRight, name, description, o
                     <h2 className={`${styles.nameRight}`}>{name}</h2>
                     <div className={`${styles.content}`}>
                         <p className={`${styles.descriptionLeft}`}>{description}</p>
-                        <ComputerWindow className={`${styles.window}`} topbarColor={color}></ComputerWindow>
+                        <ComputerWindow className={`${styles.window}`} topbarColor={color}>
+                            <img src={img}></img>
+                        </ComputerWindow>
                     </div>
                     <p className={`${styles.officersLeft}`} style={{backgroundColor: bColor}}>{officers}</p>
                 </>
