4c0a4ba83db3b722a5968ec0cee0f3a45092ce4c
Firmiana Wang <firmianaw@gmail.com>
2024-03-10 13:01:06 -0500

    fix text

--- src/pages/officers.js
+++ src/pages/officers.js
@@ -6,7 +6,8 @@ import styles from "@/styles/Officers.module.css";
 export default function Officers({children}) {
    return (
       <div className={`${styles.main}`}>
-      <h1 className={`${styles.officers}`}>Officers</h1>
+      <h1 className={`${styles.officers}`}>WCS Officer Board</h1>
+      <p className={`${styles.description}`}>Click on a photo to learn more about our officers!</p>
       <div className={`${styles.cards}`}>
          {officerData.admin.map((officer, index) => (
             <OfficerCard key={index} name={officer.name} position={officer.position} netid={officer.netid}></OfficerCard>
