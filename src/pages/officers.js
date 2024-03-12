36a2041ea7a955be48ee7b39991f6e8c7d4eeb5e
neha-vard <neha.vard@gmail.com>
2024-03-12 10:57:12 -0500

    Format

--- src/pages/officers.js
+++ src/pages/officers.js
@@ -3,16 +3,23 @@ import React from "react";
 import officerData from "public/assets/js/officers.json";
 import styles from "@/styles/Officers.module.css";
 
-export default function Officers({children}) {
-   return (
-      <div className={`${styles.main}`}>
+export default function Officers({ children }) {
+  return (
+    <div className={`${styles.main}`}>
       <h1 className={`${styles.officers}`}>WCS Officer Board</h1>
-      <p className={`${styles.description}`}>Click on a photo to learn more about our officers!</p>
+      <p className={`${styles.description}`}>
+        Click on a photo to learn more about our officers!
+      </p>
       <div className={`${styles.cards}`}>
-         {officerData.admin.map((officer, index) => (
-            <OfficerCard key={index} name={officer.name} position={officer.position} netid={officer.netid}></OfficerCard>
-         ))}
+        {officerData.admin.map((officer, index) => (
+          <OfficerCard
+            key={index}
+            name={officer.name}
+            position={officer.position}
+            netid={officer.netid}
+          ></OfficerCard>
+        ))}
       </div>
-      </div>
-   );
-};
+    </div>
+  );
+}
