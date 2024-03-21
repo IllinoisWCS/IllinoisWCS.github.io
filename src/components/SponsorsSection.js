cc5555e33cc3fc83124461ff634fab3e87d28bae
Jacob Edley <jac.dan@icloud.com>
2024-03-21 00:42:58 -0500

    adjust sponsor sizes and seperate tiers into seperate rows

--- src/components/SponsorsSection.js
+++ src/components/SponsorsSection.js
@@ -8,17 +8,21 @@ const SponsorsSection = () => {
       <h2 className={styles.header}>Our Sponsors</h2>
       <div className={styles.sponsorsSection}>
         <TabletComponent>
-          <div className={styles.sponsors}>
-            <Sponsor sponsor="google.png" tier="gold" />
-            <Sponsor sponsor="crowdstrike.png" tier="gold" />
-            <Sponsor sponsor="CMEgroup.jpg" tier="gold" />
-            <Sponsor sponsor="twoSigma.png" tier="gold" />
-            <Sponsor sponsor="drw.png" tier="gold" />
-            <Sponsor sponsor="omc.png" tier="gold" />
-            <Sponsor sponsor="hrt.png" tier="gold" />
-            <Sponsor sponsor="bloomberg.png" tier="bronze" />
-            <Sponsor sponsor="activeCampaign.png" tier="bronze" />
-            <Sponsor sponsor="citadel.png" tier="bronze" />
+          <div>
+            <div className={styles.sponsors}>
+              <Sponsor sponsor="google.png" tier="gold" />
+              <Sponsor sponsor="crowdstrike.png" tier="gold" />
+              <Sponsor sponsor="CMEgroup.jpg" tier="gold" />
+              <Sponsor sponsor="twoSigma.png" tier="gold" />
+              <Sponsor sponsor="drw.png" tier="gold" />
+              <Sponsor sponsor="omc.png" tier="gold" />
+              <Sponsor sponsor="hrt.png" tier="gold" />
+            </div>
+            <div className={styles.sponsors}>
+              <Sponsor sponsor="bloomberg.png" tier="bronze" />
+              <Sponsor sponsor="activeCampaign.png" tier="bronze" />
+              <Sponsor sponsor="citadel.png" tier="bronze" />
+            </div>
           </div>
         </TabletComponent>
       </div>
