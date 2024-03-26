bf78e0d81fa1f1c7a071d3e694f1029b0fd56a6a
Firmiana Wang <firmianaw@gmail.com>
2024-03-26 15:29:49 -0500

    mobile navbar added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from "@/styles/Navbar.module.css";
+import NavBarMobile from "./NavBarMobile";
+import { Navbar } from "react-bootstrap";
 
 export default function WCSNavbar() {
   const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenuOnResize = () => {
+    setIsMenuOpen(false);
+  };
+
+  const handleLinkClick = () => {
+    setIsMenuOpen(false); // Close the dropdown menu when link is clicked
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", closeMenuOnResize);
+    return () => {
+      window.removeEventListener("resize", closeMenuOnResize);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
-      <Link href="/">
-        <img
-          src="https://points.illinoiswcs.org/assets/logo-9d49d730.png"
-          width="140"
-        />
-      </Link>
-      <div className={styles.linksContainer}>
-        <NavLink href="https://points.illinoiswcs.org/" label="points" />
-        <NavLink href="/officers" label="officers" />
-        <NavLink href="/committees" label="committees" />
-        <NavLink href="/resources" label="resources" />
+        <Link className={styles.logo} href="/">
+          <img
+            src="https://points.illinoiswcs.org/assets/logo-9d49d730.png"
+            width="140"
+          />
+        </Link>
+        <div className={`${styles.linksContainer} ${isMenuOpen ? styles.show : ''}`}>
+          <NavLink href="https://points.illinoiswcs.org/" label="points" />
+          <NavLink href="/officers" label="officers" />
+          <NavLink href="/committees" label="committees" />
+          <NavLink href="/resources" label="resources" />
+        </div>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          <div className={`${styles.bar} ${isMenuOpen ? styles.open1 : ''}`}></div>
+          <div className={`${styles.bar} ${isMenuOpen ? styles.open2 : ''}`}></div>
+          <div className={`${styles.bar} ${isMenuOpen ? styles.open3 : ''}`}></div>
+        </div>
+        {isMenuOpen && <NavBarMobile />}
       </div>
-    </div>
   );
 
   function NavLink({ href, label }) {
