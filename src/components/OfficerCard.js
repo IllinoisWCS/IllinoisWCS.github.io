c32d7ace1b72a5d950546b52eab8d03c8e59f22f
Aditi Kumar <aditikumar2005@gmail.com>
2024-04-02 23:02:34 -0500

    changed modal to apply to entire officer card

--- src/components/OfficerCard.js
+++ src/components/OfficerCard.js
@@ -7,31 +7,32 @@ export default function OfficerCard({ name, position, img, netid, officer }) {
   const [show, setShow] = useState(false);
 
   const openModal = () => {
+    console.log("open modal");
     setShow(true);
   };
 
   const closeModal = () => {
     setShow(false);
+    console.log("close clicked");
   };
 
 
-  return (
-    <div className={`${styles.container}`}>
+return (
+  <>
+    <div className={`${styles.container}`} onClick={openModal}>
       <img
         className={`${styles.img}`}
-        onClick={openModal}
         src={`/assets/img/officers/${netid}.jpg`}
       ></img>
       <h2 className={`${styles.name}`}>{name}</h2>
       <p className={`${styles.position}`}>{position}</p>
-
-      <OfficerModal
-        isOpen={show}
-        closeModal={closeModal}
-        officer={officer}
-      />
-
     </div>
-    
-  );
+
+    <OfficerModal
+      isOpen={show}
+      closeModal={closeModal}
+      officer={officer}
+    />
+  </>
+);
 }
