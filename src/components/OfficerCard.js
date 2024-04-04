494d8fe6763849e24ee16438d1cc4db12c8222dc
neha-vard <neha.vard@gmail.com>
2024-04-03 22:34:50 -0500

    Change title font

--- src/components/OfficerCard.js
+++ src/components/OfficerCard.js
@@ -1,38 +1,30 @@
 import styles from "@/styles/OfficerCard.module.css";
-import OfficerModal from "@/components/OfficerModal.js"
-import { useState } from "react";
+import OfficerModal from "@/components/OfficerModal.js";
+import {useState} from "react";
 
-
-export default function OfficerCard({ name, position, img, netid, officer }) {
+export default function OfficerCard({name, position, netid, officer}) {
   const [show, setShow] = useState(false);
 
   const openModal = () => {
-    console.log("open modal");
     setShow(true);
   };
 
   const closeModal = () => {
     setShow(false);
-    console.log("close clicked");
   };
 
+  return (
+    <>
+      <div className={`${styles.container}`} onClick={openModal}>
+        <img
+          className={`${styles.img}`}
+          src={`/assets/img/officers/${netid}.jpg`}
+        ></img>
+        <h2 className={`${styles.name}`}>{name}</h2>
+        <p className={`${styles.position}`}>{position}</p>
+      </div>
 
-return (
-  <>
-    <div className={`${styles.container}`} onClick={openModal}>
-      <img
-        className={`${styles.img}`}
-        src={`/assets/img/officers/${netid}.jpg`}
-      ></img>
-      <h2 className={`${styles.name}`}>{name}</h2>
-      <p className={`${styles.position}`}>{position}</p>
-    </div>
-
-    <OfficerModal
-      isOpen={show}
-      closeModal={closeModal}
-      officer={officer}
-    />
-  </>
-);
+      <OfficerModal isOpen={show} closeModal={closeModal} officer={officer} />
+    </>
+  );
 }
