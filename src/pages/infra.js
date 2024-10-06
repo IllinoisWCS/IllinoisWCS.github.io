0105daa8510e7fcbdc8a3608dcddc2921b9e9d65
Prisha Thoguluva <prishat2@illinois.edu>
2024-10-05 23:55:29 -0500

    Added a card element in infra.js

--- src/pages/infra.js
+++ src/pages/infra.js
@@ -6,7 +6,7 @@ export default function InfraCommittee() {
         our wonderful infrastructure committee
       </h3>
       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-        {/* put something here... */}
+        <div style={{ margin: '10px', padding: '20px', border: '1px solid #ccc', width: '200px', textAlign: 'center' }}><h4>Team Member</h4><p>Role: Developer</p></div>
       </div>
     </div>
   );
