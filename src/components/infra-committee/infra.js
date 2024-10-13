35730578ed2b167c29733904ddc77e5a03aade39
Prisha Thoguluva <prishat2@illinois.edu>
2024-10-13 10:07:32 -0500

    Updated InfraCommittee component with placeholder text

---
+++ src/components/infra-committee/infra.js
@@ -0,0 +1,27 @@
+// import styles from '../styles/components/InfraCommittee.modules.css';
+
+export default function InfraCommittee() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Meet the Team!</h1>
+      <h3 style={{ textAlign: 'center' }}>
+        our wonderful infrastructure committee
+      </h3>
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+          <p>Team member profiles!</p>
+        <div
+          style={{
+            margin: '10px',
+            padding: '20px',
+            border: '1px solid #ccc',
+            width: '200px',
+            textAlign: 'center',
+          }}
+        >
+          <h4>Team Member</h4>
+          <p>Role: Developer</p>
+        </div>
+      </div>
+    </div>
+  );
+}
