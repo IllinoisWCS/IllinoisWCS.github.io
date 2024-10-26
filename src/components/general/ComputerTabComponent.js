80faa240affc22b2ab867aa91d0c650483e93e49
Nyssa Aftab <nyssaa2@illinois.edu>
2024-10-26 15:31:28 -0500

    created computertab component

---
+++ src/components/general/ComputerTabComponent.js
@@ -0,0 +1,15 @@
+import styles from '@/styles/components/ComputerWindow.module.css';
+
+export default function ComputerWindow({
+  children,
+  className,
+  topbarColor = '#65C7CC',
+}) {
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <div className={styles.topbar} style={{ backgroundColor: topbarColor }}>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+}
