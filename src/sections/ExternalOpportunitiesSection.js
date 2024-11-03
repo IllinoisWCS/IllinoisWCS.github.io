8b12a042bd1849056a12a443a76402fd1eec76ef
Skyla Jin <skylajin@gmail.com>
2024-11-03 02:29:05 -0600

    add external opportunities grid on resources page

--- src/sections/ExternalOpportunitiesSection.js
+++ src/sections/ExternalOpportunitiesSection.js
@@ -1,49 +1,36 @@
-import Cloud from '../components/CloudComponent';
+import ExternalOpportunityCategoryCard from '../components/ExternalOpportunityCategoryCard';
 import styles from '@/styles/sections/ExternalOpportunitiesSection.module.css';
+
+const cardsData = [
+  { title: 'Conferences and Events', topbarColor: '#E2626A' },
+  { title: 'Corporate', topbarColor: '#94d1c9' },
+  { title: 'Grad School', topbarColor: '#d88181' },
+  { title: 'RSO', topbarColor: '#597d96' },
+  { title: 'Research and Focus Group', topbarColor: '#9ed8b7' },
+  { title: 'Scholarships', topbarColor: '#c292d8' },
+  { title: 'Workshops and Education', topbarColor: '#f6b6c0' },
+  { title: 'Non-Profit', topbarColor: '#8e61b0' },
+  { title: 'Volunteering', topbarColor: '#8ebdf0' },
+];
+
+// will have to change later; just a placeholder
+const opportunitiesData = [
+  { title: 'UX Days at Siebel Center for Design', emoji: '🎨' },
+  { title: 'UX Days at Siebel Center for Design', emoji: '🎨' },
+  { title: 'UX Days at Siebel Center for Design', emoji: '🎨' },
+];
 
 export default function ExternalOpportunitiesSection() {
   return (
     <div className={styles.container}>
-      <div className={styles.mainCloud}>
-        <Cloud color="pink" size="large" />
-      </div>
-
-      <div className={styles.cloudText}>
-        <p>
-          Coming soon!
-          <br />
-          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-          Check out {/* eslint-disable-next-line react/jsx-no-target-blank */}
-          <a
-            href="https://illinoiswcs.notion.site/External-Opportunities-Board-55cf543f69934c79b46a5df5dbec1512"
-            target="_blank"
-          >
-            <u>this link</u>
-            {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-          </a>{' '}
-          for now.
-        </p>
-      </div>
-
-      <div className={styles.cloud1}>
-        <Cloud color="pink" size="medium" />
-      </div>
-
-      <div className={styles.cloud2}>
-        <Cloud color="pink" size="small" />
-      </div>
-
-      <div className={styles.cloud3}>
-        <Cloud color="pink" size="small" />
-      </div>
-
-      <div className={styles.cloud4}>
-        <Cloud color="pink" size="small" />
-      </div>
-
-      <div className={styles.cloud5}>
-        <Cloud color="pink" size="medium" />
-      </div>
+      {cardsData.map((card, index) => (
+        <ExternalOpportunityCategoryCard
+          key={index}
+          categoryName={card.title}
+          topbarColor={card.topbarColor}
+          items={opportunitiesData}
+        />
+      ))}
     </div>
   );
 }
