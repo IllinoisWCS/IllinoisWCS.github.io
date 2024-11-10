cf0972f5cdc295520c02c21e33027fe52fead9f2
Nyssa Aftab <nyssaa2@illinois.edu>
2024-11-09 20:56:40 -0600

    finished changes to aboutus section and fixed computerwindow linter errors

--- src/sections/AboutUsSection.js
+++ src/sections/AboutUsSection.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import PhoneComponent from '../components/general/PhoneComponent';
 import ComputerWindow from '../components/general/ComputerWindowComponent';
-import ComputerTab from '../components/general/ComputerTabComponent';
 import styles from '@/styles/sections/AboutUsSection.module.css';
 
 export default function AboutUsSection() {
@@ -9,57 +8,61 @@ export default function AboutUsSection() {
     <div className={`${styles.container}`}>
       <div className={styles.computerWindowContainer}>
         <ComputerWindow className={styles.window}>
-          <h2>Illinois Women in Computer Science</h2>
+          <div className={styles.windowContent}>  
+            <h1>Illinois Women in Computer Science</h1>
+          </div>
         </ComputerWindow>
       </div>
 
       <div className={styles.main}>
         <div className={styles.text}>
-          <div className={styles.computerTabContainer}>
-            <ComputerTab>
-              <div className={styles.panelContainer}>
-                <div className={styles.leftPanel}>
-                  <h3>We are...</h3>
-                  <ul>
-                    <li>
-                      a non-profit educational student group under the UIUC
-                      Computer Science Department
-                    </li>
-                    <li>
-                      dedicated to supporting the efforts of young women and
-                      non-binary people pursuing careers in CS or show interests
-                      in technology
-                    </li>
-                  </ul>
-                </div>
-
-                <div className={styles.rightPanel}>
-                  <h3>We host...</h3>
-                  <ul>
-                    <li>
-                      networking and social events with our corporate sponsors
-                    </li>
-                    <li>
-                      workshops and discussions on topics from technical skills
-                      to professional and academic advice
-                    </li>
-                    <li>
-                      community building events for our members to have fun
-                    </li>
-                    <li>and many more events!</li>
-                  </ul>
-                </div>
+          <ComputerWindow
+            className={styles.aboutContainer}
+            topbarColor="wcs-pink"
+            showButtons={false}
+          >
+            <div className={styles.panelContainer}>
+              <div className={styles.leftPanel}>
+                <h3>We are...</h3>
+                <ul>
+                  <li>
+                    a non-profit educational student group under the UIUC
+                    Computer Science Department
+                  </li>
+                  <li>
+                    dedicated to supporting the efforts of young women and
+                    non-binary people pursuing careers in CS or show interests
+                    in technology
+                  </li>
+                </ul>
               </div>
-            </ComputerTab>
-          </div>
 
-          <div className={styles.diversityContainer}>
-            <ComputerTab>
-              <p className={styles.diversityStatement}>
-                Blah blah diversity statement here
-              </p>
-            </ComputerTab>
-          </div>
+              <div className={styles.rightPanel}>
+                <h3>We host...</h3>
+                <ul>
+                  <li>
+                    networking and social events with our corporate sponsors
+                  </li>
+                  <li>
+                    workshops and discussions on topics from technical skills to
+                    professional and academic advice
+                  </li>
+                  <li>community building events for our members to have fun</li>
+                  <li>and many more events!</li>
+                </ul>
+              </div>
+            </div>
+          </ComputerWindow>
+
+          <ComputerWindow
+            className={styles.diversityContainer}
+            topbarColor="wcs-pink"
+            showButtons={false}
+          >
+            <p className={styles.diversityStatement}>
+              Blah blah diversity statement here
+            </p>
+          </ComputerWindow>
         </div>
 
         <div className={`${styles.phoneContainer}`}>
