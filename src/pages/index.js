2cec27013802bf437f2fea845f5495d99f559b83
Becky Blake <ryblake2@illinois.edu>
2024-11-11 17:36:35 -0600

    Add footer (#334)
    * add footer
    
    * change h3 to div in key component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 
 import AboutUsSection from '../sections/AboutUsSection';
