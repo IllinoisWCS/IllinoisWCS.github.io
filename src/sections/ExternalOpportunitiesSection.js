0026b4fd3ca9ac1db39e6b937da5b412db5e80ff
kikis05 <kirthis2@illinois.edu>
2024-11-13 18:49:14 -0600

    express + data access functions in ExternalOpps.js + packages were added

--- src/sections/ExternalOpportunitiesSection.js
+++ src/sections/ExternalOpportunitiesSection.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import ExternalOpportunityCategoryCard from '../components/ExternalOpportunityCategoryCard';
 import styles from '@/styles/sections/ExternalOpportunitiesSection.module.css';
 
@@ -21,16 +22,124 @@ const opportunitiesData = [
 ];
 
 export default function ExternalOpportunitiesSection() {
+  //  this will contain the data from notion
+  const [opportunities, setOpportunities] = useState([{}]);
+
+  //  this fetches the opportunities data and assigns it to opportunities (above)
+  //   You can use the map function to iterate through the opportunities.
+
+  useEffect(() => {
+    fetch('http://localhost:5000/external-opps-api')
+      .then((response) => response.json())
+      .then((data) => {
+        setOpportunities(data);
+      });
+  }, []);
+
+  //  returns type of opportunity (ex. Conferences and Events)
+  const getType = (properties) => properties.Type.select.name;
+
+  //  returns name/title of opportunity
+  const getName = (properties) => {
+    const name = properties.Name;
+    return name.title[0] ? name.title[0].text.content : 'No Title';
+  };
+
+  //  returns time of opportunity
+  const getTime = (properties) => {
+    const time = properties.Time;
+    return time.rich_text[0] ? time.rich_text[0].text.content : 'None';
+  };
+
+  //  returns expiration date of opportunity
+  const getExpiration = (properties) => {
+    const expires = properties.Expires;
+    return expires.date ? expires.date.start : 'NA';
+  };
+
+  //  returns location of opportunity
+  const getLocation = (properties) => {
+    const location = properties.Location;
+    return location.rich_text[0] ? location.rich_text[0].text.content : 'None';
+  };
+
+  //  returns description of opportunity (formatting of this might be wonky atm)
+  const getDescription = (properties) => {
+    const description = properties.Description;
+    if (description.rich_text[0]) {
+      return description.rich_text.map((block) => {
+        if (block.type === 'text') {
+          return block.text.content;
+        }
+        return '';
+      });
+    }
+    return 'No Description';
+  };
+
+  //  returns url
+  const getURL = (properties) => properties.Link.url;
+
+  /*
+  Please see the commented section below the return statement for an
+  example of how to use the functions above to access the opportunities data.
+  */
+
   return (
-    <div className={styles.container}>
-      {cardsData.map((card, index) => (
-        <ExternalOpportunityCategoryCard
-          key={index}
-          categoryName={card.title}
-          topbarColor={card.topbarColor}
-          items={opportunitiesData}
-        />
-      ))}
-    </div>
+    <>
+      <div className={styles.container}>
+        {cardsData.map((card, index) => (
+          <ExternalOpportunityCategoryCard
+            key={index}
+            categoryName={card.title}
+            topbarColor={card.topbarColor}
+            items={opportunitiesData}
+          />
+        ))}
+      </div>
+
+      <div>
+        {opportunities ? (
+          opportunities.map((opp) => (
+            <div key={opp.id}>
+              {opp.properties ? (
+                <>
+                  <p>{getType(opp.properties)}</p>
+                  <p>{getName(opp.properties)}</p>
+                  <p>{getTime(opp.properties)}</p>
+                  <p>{getExpiration(opp.properties)}</p>
+                  <p>{getLocation(opp.properties)}</p>
+                  <p>{getDescription(opp.properties)}</p>
+                  <p>{getURL(opp.properties)}</p>
+                </>
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+          ))
+        ) : (
+          <p>Data not loaded....</p>
+        )}
+      </div>
+    </>
   );
 }
+
+//  example of how to access data per opportunity in our database:
+
+// { (opportunities) ? (opportunities.map((opp) => (
+//   <div key = {opp.id}>
+//     {(opp.properties) ? (
+//       <>
+//         <p>{getName(opp.properties)}</p>
+//         <p>{getTime(opp.properties)}</p>
+//         <p>{getExpiration(opp.properties)}</p>
+//         <p>{getLocation(opp.properties)}</p>
+//         <p>{getDescription(opp.properties)}</p>
+//         <p>{getUrl(opp.properties)}</p>
+//       </>
+//     ) : (
+//       <p>Loading...</p>
+//     )}
+//   </div>
+// ))) : (<p>Data not loaded....</p>)}
