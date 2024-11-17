4d02ad9816a8bbb20af06481f49486e270b91aee
Nyssa Aftab <nyssaa2@illinois.edu>
2024-11-17 00:55:43 -0600

    majority of updated modal

--- src/components/OfficerModal.js
+++ src/components/OfficerModal.js
@@ -7,6 +7,7 @@ function OfficerInformation(
   closeModal,
   {
     name,
+    pronouns,
     year,
     major,
     position,
@@ -23,89 +24,119 @@ function OfficerInformation(
 ) {
   return (
     <div className={styles.container}>
-      <ComputerWindow className={styles.window} topbarColor="#D2616C">
+      <ComputerWindow
+        className={styles.window}
+        topbarColor="wcs-pink"
+        onButtonClick={closeModal}
+      >
         <div className={styles.officerInfo}>
-          <h3 className={styles.title}>
-            {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-            {name} - {position}{' '}
-          </h3>
-
           <div className="columnContainer">
             <div className={styles.left}>
-              <Image
-                className={`${styles.photo}`}
-                src={`/assets/img/officers/${netid}.jpg`}
-                width={400}
-                height={400}
-                alt={name}
-              />
+              <div className={styles.photoContainer}>
+                <Image
+                  className={`${styles.photo}`}
+                  src={`/assets/img/officers/${netid}.jpg`}
+                  width={400}
+                  height={400}
+                  alt={name}
+                />
+              </div>
+
+              <div className={styles.links}>
+                <a href={`mailto:${email}`}>
+                  <Image
+                    className={`${styles.socials}`}
+                    src="/assets/img/logos/media/email.png"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    alt="email logo"
+                  />
+                  
+                </a>
+                <a href={linkedin}>
+                  <Image
+                    className={`${styles.socials}`}
+                    src="/assets/img/logos/media/linkedin.png"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    alt="linkedin logo"
+                  />
+                </a>
+              </div>
             </div>
 
             <div className={styles.right}>
-              <p>
-                <b>Major: </b>
-                {major}
-              </p>
-              <p>
-                <b>Year: </b>
-                {year}
-              </p>
-              <p>
-                <b>From: </b>
-                {place}
-              </p>
-              <p>
-                <b>Involvements: </b>
-                {involvements}
-              </p>
-              <p>
-                <b>Interests: </b>
-                {interests}
-              </p>
-              <p>
-                <b>Hobbies: </b>
-                {hobbies}
-              </p>
-              <p>
-                <b>Fun Fact: </b>
-                {fact}
-              </p>
-              <p>
-                <b>Advice: </b>
-                {advice}
-              </p>
+              <h1 className={styles.title}>{name}</h1>
+
+              <div className={styles.list}>
+                <p>
+                  <b>Position: </b>
+                  {position}
+                </p>
+                {/* <p>
+                <b>Pronouns: </b>
+                {pronouns}
+              </p> */}
+                <p>
+                  <b>Major: </b>
+                  {major}
+                </p>
+                <p>
+                  <b>Year: </b>
+                  {year}
+                </p>
+                <p>
+                  <b>From: </b>
+                  {place}
+                </p>
+                <p>
+                  <b>Involvements: </b>
+                  {involvements}
+                </p>
+                <p>
+                  <b>Interests: </b>
+                  {interests}
+                </p>
+                <p>
+                  <b>Hobbies: </b>
+                  {hobbies}
+                </p>
+                <p>
+                  <b>Fun Fact: </b>
+                  {fact}
+                </p>
+                <p>
+                  <b>Advice: </b>
+                  {advice}
+                </p>
+
+              </div>
+
+              <div className={styles.closeButtonContainer}>
+                <button
+                type="button"
+                onClick={closeModal}
+                className={styles.closeButton}
+              >
+                Close
+              </button>
+              </div>
+
               <br />
 
-              <a href={`mailto:${email}`}>
-                <Image
-                  className={`${styles.socials}`}
-                  src="/assets/img/logos/media/email.png"
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  alt="email logo"
-                />
-              </a>
-              <a href={linkedin}>
-                <Image
-                  className={`${styles.socials}`}
-                  src="/assets/img/logos/media/linkedin.png"
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  alt="linkedin logo"
-                />
-              </a>
+              
             </div>
           </div>
         </div>
-        <button
+        {/* <button
           type="button"
           onClick={closeModal}
           className={styles.closeButton}
         >
           Close
-        </button>
+        </button> */}
       </ComputerWindow>
     </div>
   );
