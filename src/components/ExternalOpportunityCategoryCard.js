67f6b9f4eeddc7010d000ae225dd0ff4aea8b0e8
ziaxlu <124220349+ziaxlu@users.noreply.github.com>
2024-11-18 11:56:19 -0800

    Create External Opportunities Popup Modal (#336)
    * Make external opportunities modal
    
    * Remove comments
    
    * Render colors dynamically
    
    * Change modal to single component
    
    * Remove comments
    
    * Fix card styling
    
    * Fix opportunity card styling

--- src/components/ExternalOpportunityCategoryCard.js
+++ src/components/ExternalOpportunityCategoryCard.js
@@ -1,26 +1,72 @@
+import React, { useState } from 'react';
 import ComputerWindow from './general/ComputerWindowComponent';
+import ExternalOpportunitiesModal from './events/ExternalOpportunitiesModal';
 import styles from '@/styles/components/ExternalOpportunityCategoryCard.module.css';
+
+const colorOptions = {
+  'wcs-pink': '#e2626a',
+  'wcs-blue': '#69c7cb',
+};
 
 export default function ExternalOpportunityCategoryCard({
   categoryName,
   topbarColor,
   items,
 }) {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
+
+  const filteredEvents = items.filter(
+    (event) => event.category === categoryName,
+  );
+
+  const blankLinesCount = 3 - filteredEvents.length;
+
+  const displayEvents = [
+    ...filteredEvents,
+    ...Array(blankLinesCount).fill({ title: '', icon: '' }), // Blank items for the remaining space
+  ];
+
   return (
     <div className={styles.card}>
-      <ComputerWindow className={styles.window} topbarColor={topbarColor}>
+      <ComputerWindow
+        className={styles.window}
+        topbarColor={colorOptions[topbarColor]}
+      >
         <div className={styles.container}>
           <h3>{categoryName}</h3>
           <div className={styles.separator} />
           <ul className={styles.itemsList}>
-            {items.map((item, index) => (
+            {displayEvents.map((item, index) => (
               <li key={index}>
-                {item.emoji} {item.title}
+                {item.icon && item.title ? (
+                  <>
+                    {item.icon} {item.title}
+                  </>
+                ) : (
+                  <span>&nbsp;</span>
+                )}
               </li>
             ))}
           </ul>
-          <div className={styles.viewMoreButton}>
-            <p>View more</p>
+          <div>
+            <button
+              className={styles.viewMoreButton}
+              type="button"
+              onClick={toggleModal}
+            >
+              View more
+            </button>
+            <ExternalOpportunitiesModal
+              color={topbarColor}
+              category={categoryName}
+              events={items}
+              isOpen={isModalOpen}
+              closeModal={toggleModal}
+            />
           </div>
         </div>
       </ComputerWindow>
