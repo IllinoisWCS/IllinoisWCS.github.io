773031510a396085fc3cb1bf2f96264cb581695a
kikis05 <kirthis2@illinois.edu>
2024-12-29 18:07:24 -0600

    added a time filter

--- src/server/server.js
+++ src/server/server.js
@@ -11,6 +11,8 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const app = express();
 
+const moment = require('moment');
+
 // "@notionhq/client": "^2.2.15",
 // "body-parser": "^1.20.3",
 // "cors": "^2.8.5",
@@ -26,12 +28,22 @@ const notion = new Client({
   auth: process.env.REACT_APP_NOTION_API_KEY,
 });
 
+async function filterRecentOpportunities(data) {
+  const curr = moment().subtract(1, 'days');
+  const filteredData = data.filter(
+    (page) =>
+      page.properties.Expires.date &&
+      moment(page.properties.Expires.date.start).isAfter(curr),
+  );
+  return filteredData;
+}
+
 app.get('/external-opps-api', jsonParser, async (req, res) => {
   const results = await notion.databases.query({
     database_id: process.env.REACT_APP_NOTION_DATABASE_ID,
   });
-
-  res.json(results.results);
+  const tempRes = await filterRecentOpportunities(results.results);
+  res.json(tempRes);
 });
 
 app.listen(PORT, HOST, () => {});
