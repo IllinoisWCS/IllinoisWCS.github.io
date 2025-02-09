1ee4bee58f22ddc0b0338704df8a890246d4743d
zlu <ziaxxlu@gmail.com>
2025-02-09 15:05:30 -0600

    Use functions to get data

--- src/server/server.js
+++ src/server/server.js
@@ -28,6 +28,52 @@ const notion = new Client({
   auth: process.env.REACT_APP_NOTION_API_KEY,
 });
 
+// returns category
+const getType = (properties) => properties.Type.multi_select[0].name;
+
+//  returns name/title of opportunity
+const getName = (properties) => {
+  const name = properties.Name;
+  return name ? name.title[0].text.content : '';
+};
+
+//  returns time of opportunity
+const getTime = (properties) => {
+  const time = properties.Time;
+  return time.rich_text[0] ? time.rich_text[0].text.content : 'No Time';
+};
+
+//  returns expiration date of opportunity
+const getExpiration = (properties) => {
+  const expires = properties.Expires;
+  return expires.date ? expires.date.start : 'No Expiration';
+};
+
+//  returns location of opportunity
+const getLocation = (properties) => {
+  const location = properties.Location;
+  return location.rich_text[0]
+    ? location.rich_text[0].text.content
+    : 'No Location';
+};
+
+//  returns description of opportunity (formatting of this might be wonky atm)
+const getDescription = (properties) => {
+  const description = properties.Description;
+  if (description.rich_text[0]) {
+    return description.rich_text.map((block) => {
+      if (block.type === 'text') {
+        return block.text.content;
+      }
+      return '';
+    });
+  }
+  return 'No Description';
+};
+
+//  returns url
+const getURL = (properties) => properties.Link.url;
+
 async function filterRecentOpportunities(data) {
   const curr = moment().subtract(1, 'days');
   // Directly return the filtered data without using a block
@@ -48,17 +94,15 @@ app.get('/external-opps-api', jsonParser, async (req, res) => {
 
   const tempRes = filteredRes.map((item) => ({
     icon: item.icon?.emoji || '💡', // eslint-disable-line operator-linebreak
-    title: item.properties.Name.title[0]?.text?.content || 'No Title', // eslint-disable-line operator-linebreak
-    location:
-      item.properties.Location.rich_text[0]?.text?.content || 'No Location', // eslint-disable-line operator-linebreak
-    date: item.properties.Expires.date.start || 'No Expiry Date', // eslint-disable-line operator-linebreak
-    time: item.properties.Time.rich_text[0]?.text?.content || 'No Time', // eslint-disable-line operator-linebreak
+    title: getName(item.properties), // eslint-disable-line operator-linebreak
+    location: getLocation(item.properties), // eslint-disable-line operator-linebreak
+    date: item.properties.Expires.date.start, // eslint-disable-line operator-linebreak
+    time: getTime(item.properties), // eslint-disable-line operator-linebreak
     description:
       // eslint-disable-next-line max-len
-      item.properties.Description.rich_text[0]?.text?.content || // eslint-disable-line operator-linebreak
-      'No Description',
-    link: item.properties.Link.url || '', // eslint-disable-line operator-linebreak
-    category: item.properties.Type.multi_select[0]?.name || 'No Category', // eslint-disable-line operator-linebreak
+      getDescription(item.properties), // eslint-disable-line operator-linebreak
+    link: getURL(item.properties), // eslint-disable-line operator-linebreak
+    category: getType(item.properties), // eslint-disable-line operator-linebreak
   }));
 
   res.json(tempRes);
