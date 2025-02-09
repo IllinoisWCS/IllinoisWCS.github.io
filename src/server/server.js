6d459ae7feaa33a5f8c8e8075c9b0ccb25df5972
zlu <ziaxxlu@gmail.com>
2025-02-09 13:30:24 -0600

    Formatting

--- src/server/server.js
+++ src/server/server.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const express = require('express');
 
 const { Client } = require('@notionhq/client');
-
 const cors = require('cors');
-
 const bodyParser = require('body-parser');
 
 const jsonParser = bodyParser.json();
@@ -14,7 +12,6 @@ const app = express();
 const moment = require('moment');
 
 const fs = require('fs');
-
 
 // "@notionhq/client": "^2.2.15",
 // "body-parser": "^1.20.3",
@@ -31,15 +28,15 @@ const notion = new Client({
   auth: process.env.REACT_APP_NOTION_API_KEY,
 });
 
-
 async function filterRecentOpportunities(data) {
   const curr = moment().subtract(1, 'days');
-  const filteredData = data.filter(
+  // Directly return the filtered data without using a block
+  return data.filter(
     (page) =>
-      page.properties.Expires.date &&
+      // eslint-disable-next-line implicit-arrow-linebreak
+      page.properties.Expires.date && // eslint-disable-line operator-linebreak
       moment(page.properties.Expires.date.start).isAfter(curr),
   );
-  return filteredData;
 }
 
 app.get('/external-opps-api', jsonParser, async (req, res) => {
@@ -47,28 +44,34 @@ app.get('/external-opps-api', jsonParser, async (req, res) => {
     database_id: process.env.REACT_APP_NOTION_DATABASE_ID,
   });
 
-  const filteredRes = await filterRecentOpportunities(results.results)
-  
-  const tempRes = filteredRes.map(item => {
-    return {
-      icon: icon = item.icon?.emoji || "💡",
-      title: item.properties.Name.title[0]?.text?.content || "No Title",
-      location: item.properties.Location.rich_text[0]?.text?.content || "No Location",
-      date: item.properties.Expires.date.start || "No Expiry Date",
-      time: item.properties.Time.rich_text[0]?.text?.content || "No Time",
-      description: item.properties.Description.rich_text[0]?.text?.content || "No Description",
-      link: item.properties.Link.url || "",
-      category: item.properties.Type.multi_select[0]?.name || "No Category",
-    };
-  });
+  const filteredRes = await filterRecentOpportunities(results.results);
+
+  const tempRes = filteredRes.map((item) => ({
+    icon: item.icon?.emoji || '💡', // eslint-disable-line operator-linebreak
+    title: item.properties.Name.title[0]?.text?.content || 'No Title', // eslint-disable-line operator-linebreak
+    location:
+      item.properties.Location.rich_text[0]?.text?.content || 'No Location', // eslint-disable-line operator-linebreak
+    date: item.properties.Expires.date.start || 'No Expiry Date', // eslint-disable-line operator-linebreak
+    time: item.properties.Time.rich_text[0]?.text?.content || 'No Time', // eslint-disable-line operator-linebreak
+    description:
+      // eslint-disable-next-line max-len
+      item.properties.Description.rich_text[0]?.text?.content || // eslint-disable-line operator-linebreak
+      'No Description',
+    link: item.properties.Link.url || '', // eslint-disable-line operator-linebreak
+    category: item.properties.Type.multi_select[0]?.name || 'No Category', // eslint-disable-line operator-linebreak
+  }));
 
   res.json(tempRes);
 
-  fs.writeFile('src/data/externalOpportunities.json', JSON.stringify(tempRes), err => {
-    if (err) {
-      console.error(err);
-    }
-  });
+  fs.writeFile(
+    'src/data/externalOpportunities.json',
+    JSON.stringify(tempRes),
+    (err) => {
+      if (err) {
+        console.error(err); // eslint-disable-line no-console
+      }
+    },
+  );
 });
 
 app.listen(PORT, HOST, () => {});
