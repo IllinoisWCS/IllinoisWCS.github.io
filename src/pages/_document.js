58e5990ed3499ad9df62acec58af524b89fcc9d9
Becky Blake <ryblake2@illinois.edu>
2025-04-09 14:18:15 -0500

    update font styles

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,6 +24,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Zilla+Slab:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
