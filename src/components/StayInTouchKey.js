10417da2f0f9fea422ea0d8ff3de51b57e316331
Madison <128331362+madison01108@users.noreply.github.com>
2025-04-15 22:52:33 -0500

    Ml/openofficesfont (#382)
    * open offices font
    
    * Officer Card Name/Role Overlap (#378)
    
    * fix overlap on officer cards
    
    * shorten infrastructure to infra
    
    * create 404 page (#379)
    
    * create 404 page
    
    * make font smaller and closer to img
    
    * make officer modal responsive (#388)
    
    * make officer modal responsive
    
    * remove useEffect
    
    * Remove Animations From Team Page (#381)
    
    * remove animations from team page
    
    * add back caption and img overlap
    
    * removed arrows from open office modal (#385)
    
    * Mobile Responsiveness For OO Modal (#375)
    
    * make oo modal text bigger on mobile
    
    * reduce height to fit content on mobile
    
    * add spacing between bullets on mobile
    
    * decrease appointment button padding
    
    * add officer appt form
    
    * Mobile Responsiveness for Get Help Section (#389)
    
    * create mobile container for get help section
    
    * move chevrons further to side, center text between
    
    * fix runtime error (#391)
    
    * Open office
    
    * moved arrows (#394)
    
    * Shift Up Binary SVG on Home Page (#393)
    
    * shift up binary svg
    
    * decrease the overlap
    
    * change computer window button colors (#396)
    
    * Pt/html bug (#387)
    
    * html bug
    
    * fix bug
    
    * added tooltip prop to key, hover text for socials (#399)
    
    * Update Font Styles (#397)
    
    * update font styles
    
    * change text tags
    
    * remove old fonts
    
    * change resources header to h1
    
    * make headers consistent across pages
    
    * add feedback and report forms to footer (#383)
    
    * added feedback form
    
    * added incident report
    
    * added key for incident report
    
    * moved incident report to resources
    
    * removed period
    
    * new open office font
    
    * Ml/newclubphoto (#398)
    
    * new committee pic
    
    * Change main picture
    
    * Pt/colorfix (#392)
    
    * color bug
    
    * color fix
    
    * make header and footer colors consistent
    
    * final changes1
    
    * open offices font
    
    * Open office
    
    * new open office font
    
    ---------
    
    Co-authored-by: Becky Blake <ryblake2@illinois.edu>
    Co-authored-by: nyssaaftab <143031426+nyssaaftab@users.noreply.github.com>
    Co-authored-by: prishat2005 <145813265+prishat2005@users.noreply.github.com>
    Co-authored-by: Saloni Vaishnav <saloniv2@illinois.ed>

--- src/components/StayInTouchKey.js
+++ src/components/StayInTouchKey.js
@@ -11,23 +11,23 @@ export default function Key({ children, url, tooltip }) {
       } // eslint-disable-line indent
     : null;
 
-    return (
-      <div className={styles.tooltipWrapper}>
-        <button
-          type="button"
-          onClick={link}
-          onKeyDown={link}
-          className={styles.keyTop}
-        >
-          <div className={styles.keyBottom}>
-            <div className={styles.keyRight}>
-              <div className={styles.keyBody}>
-                <div className={styles.content}>{children}</div>
-              </div>
+  return (
+    <div className={styles.tooltipWrapper}>
+      <button
+        type="button"
+        onClick={link}
+        onKeyDown={link}
+        className={styles.keyTop}
+      >
+        <div className={styles.keyBottom}>
+          <div className={styles.keyRight}>
+            <div className={styles.keyBody}>
+              <div className={styles.content}>{children}</div>
             </div>
           </div>
-        </button>
-        {tooltip && <span className={styles.tooltipText}>{tooltip}</span>}
-      </div>
-    );
+        </div>
+      </button>
+      {tooltip && <span className={styles.tooltipText}>{tooltip}</span>}
+    </div>
+  );
 }
