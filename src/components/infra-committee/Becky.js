810d13550e7d421320f5206eca2fb00d466ffab0
Becky Blake <ryblake2@illinois.edu>
2025-10-11 22:31:28 -0500

    add fun fact + flip transition

--- src/components/infra-committee/Becky.js
+++ src/components/infra-committee/Becky.js
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from '@/styles/components/InfraCommittee/Becky.module.css';
 
 export default function BeckyCard() {
+  const [flipped, setFlipped] = useState(false);
   return (
-    <div className={`${styles.container}`}>
-      <Image
-        className={`${styles.img}`}
-        src="/assets/img/infra-committee/Becky_Blake.png"
-        width={200}
-        height={200}
-      />
+    <button
+      type="button"
+      className={styles.flipCard}
+      onClick={() => setFlipped(!flipped)}
+    >
+      <div
+        className={`${styles.flipCardInner} ${flipped ? styles.flipped : ''}`}
+      >
+        <div className={styles.flipCardFront}>
+          <Image
+            className={styles.img}
+            src="/assets/img/infra-committee/Becky_Blake.png"
+            width={200}
+            height={200}
+          />
+          <h4 className={styles.name}>Becky Blake</h4>
+        </div>
 
-      <div>
-        <h4 className={`${styles.name}`}>Becky Blake</h4>
+        <div className={styles.flipCardBack}>
+          <h4 className={styles.funFact}>
+            Fun Fact: I had a goldfish that lived for 8 years
+          </h4>
+        </div>
       </div>
-    </div>
+    </button>
   );
 }
