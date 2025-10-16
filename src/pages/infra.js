afc9d68dc896fcf44f63c9c9fd80b0c34f180846
avaya123 <avaya.a07@gmail.com>
2025-10-15 23:25:17 -0500

    Add Avaya card and image for Infra Committee page

--- src/pages/infra.js
+++ src/pages/infra.js
@@ -1,5 +1,6 @@
 import ComputerWindow from '../components/general/ComputerWindowComponent';
 import styles from '@/styles/pages/Infra.module.css';
+import Avaya from "../components/infra-committee/Avaya";
 
 export default function Infra() {
   return (
@@ -50,7 +51,7 @@ export default function Infra() {
         <h2>2025-2026</h2>
       </ComputerWindow>
       <div className={styles.cards}>
-        <p>This is where you&apos;ll put stuff (Delete this tag later)</p>
+        <Avaya />
       </div>
     </div>
   );
