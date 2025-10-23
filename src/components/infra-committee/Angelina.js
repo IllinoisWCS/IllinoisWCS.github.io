5c5e368c05b9ff18f4e4e531b130ec85ddca0a63
angelinaezhou <aezhou721@gmail.com>
2025-10-23 17:11:23 -0500

    added Angelina card unique features

--- src/components/infra-committee/Angelina.js
+++ src/components/infra-committee/Angelina.js
@@ -3,33 +3,37 @@ import Image from 'next/image';
 import styles from '@/styles/components/InfraCommittee/Angelina.module.css';
 
 export default function AngelinaCard() {
-  const [flipped, setFlipped] = useState(false);
-  return (
-    <button
-      type="button"
-      className={styles.azflipCard}
-      onClick={() => setFlipped(!flipped)}
-    >
-      <div
-        className={`${styles.azflipCardInner} ${flipped ? styles.azflipped : ''}`}
-      >
-        <div className={styles.azflipCardFront}>
-          <Image
-            className={styles.azimg}
-            src="/assets/img/infra-committee/Angelina_Zhou.jpg"
-            width={200}
-            height={200}
-          />
-          <h4 className={styles.azname}>Angelina Zhou</h4>
-        </div>
+  const name = 'Angelina Zhou';
+  const funFact =
+    "Fun fact: I compete on Illinois' Intercollegiate Figure Skating Team!";
 
-        <div className={styles.azflipCardBack}>
-          <h4 className={styles.azfunFact}>
-            Fun Fact: I compete on Illinois&apos; Intercollegiate Figure Skating
-            Team!
-          </h4>
-        </div>
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div className={styles.azcard}>
+      <Image
+        className={styles.azphoto}
+        src="/assets/img/infra-committee/Angelina_Zhou.jpg"
+        width={130}
+        height={160}
+        alt="Angelina Zhou"
+      />
+      <div
+        className={styles.aztext}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        <h3
+          className={`${styles.azname} ${hovered ? styles.azfadeOut : styles.azfadeIn}`}
+        >
+          {name}
+        </h3>
+        <h3
+          className={`${styles.azname} ${hovered ? styles.azfadeIn : styles.azfadeOut}`}
+        >
+          {funFact}
+        </h3>
       </div>
-    </button>
+    </div>
   );
 }
