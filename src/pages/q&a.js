a9b601ae768370c3e5d3ea2cbe8f9d2bd7569837
Becky Blake <ryblake2@illinois.edu>
2025-11-20 19:32:43 -0600

    add toast notifs

--- src/pages/q&a.js
+++ src/pages/q&a.js
@@ -1,11 +1,61 @@
-import QASpeechBubble from '@/components/general/qa-forum/QASpeechBubble';
-import QAInputBox from '@/components/general/qa-forum/QAInputBox';
+import { toast } from 'react-toastify';
+// import QASpeechBubble from '@/components/general/qa-forum/QASpeechBubble';
+// import QAInputBox from '@/components/general/qa-forum/QAInputBox';
 import ComputerWindow from '../components/general/ComputerWindowComponent';
 import QuestionStatusToggle from '../components/general/qa-forum/QuestionStatusToggle';
 import styles from '@/styles/pages/Q&A.module.css';
-import QuestionAccordion from '@/components/general/qa-forum/QuestionAccordian';
+// import QuestionAccordion from '@/components/general/qa-forum/QuestionAccordian';
+import QASubmitButton from '../components/general/qa-forum/QASubmitButton';
+
+const toastSuccess = (msg) => {
+  toast.success(`🦄 ${msg}`, {
+    position: 'top-right',
+    autoClose: 2500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored',
+  });
+};
+
+const toastError = (msg) => {
+  toast.error(msg, {
+    position: 'top-right',
+    autoClose: 2500,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored',
+  });
+};
 
 export default function QA() {
+  const submitQuestion = () => {
+    const success = true; // We can call api here and check if successful or not
+    if (success) {
+      toastSuccess('Your question has been submitted!');
+    } else {
+      toastError(
+        'There was an error submitting your question. Please try again.', // TODO: change message based on error type
+      );
+    }
+  };
+
+  const submitAnswer = () => {
+    const success = true; // Simulate success or failure
+    if (success) {
+      toastSuccess('Your answer has been submitted!');
+    } else {
+      toastError(
+        'There was an error submitting your answer. Please try again.', // TODO: change message based on error type
+      );
+    }
+  };
+
   return (
     <main className={`${styles.main}`}>
       <div className={`${styles.container}`}>
@@ -27,6 +77,8 @@ export default function QA() {
 
         <div className={styles.main}>
           <QuestionStatusToggle />
+          <QASubmitButton onClick={submitQuestion} />
+          <QASubmitButton onClick={submitAnswer} />
         </div>
       </div>
     </main>
