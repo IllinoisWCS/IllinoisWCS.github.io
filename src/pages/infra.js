7608d155680d2a21564caf347be870f0dc5b6b0a
kikis05 <kirthis2@illinois.edu>
2025-12-03 16:50:21 -0600

    formatting

--- src/pages/infra.js
+++ src/pages/infra.js
@@ -15,45 +15,6 @@ import Avaya from '../components/infra-committee/Avaya';
 export default function Infra() {
   return (
     <div className={styles.main}>
-      <ComputerWindow className={styles.instructions} showTopbar={false}>
-        <p>Congrats! You found the onboarding assignment!</p>
-        <p>
-          If you haven&apos;t already, git checkout -b
-          yourinitials/onboarding-assignment so that your work is on a new
-          branch.
-        </p>
-        <ul>
-          <li>
-            Navigate to <code>src/components/infra-committee</code>
-          </li>
-          <li>
-            Make a new file, call it <code>YourName.js</code>. Write the code
-            for your card here! Be as creative as you&apos;d like.
-          </li>
-          <li>
-            Feel free to reference/use the card component used in the Our Teams
-            page on the website. Try to find this yourself!
-          </li>
-          <li>
-            Navigate to <code>src/styles/components/InfraCommittee</code>. You
-            can make a file for your card&apos;s CSS here if you are
-            comfortable!
-          </li>
-          <li>
-            Navigate to <code>public/assets/img/infra-committee</code>, store
-            your photo here.
-          </li>
-          <li>
-            Import your new component in this file, and add it as a child of the
-            div with the class name &quot;cards&quot;.
-          </li>
-          <li>
-            Create a PR for your changes. Check out the github tutorial for how
-            to do this.
-          </li>
-        </ul>
-        <p>Feel free to ask us any questions.</p>
-      </ComputerWindow>
       <ComputerWindow className={styles.title} showButtons={false}>
         <h1>Infrastructure Committee</h1>
       </ComputerWindow>
