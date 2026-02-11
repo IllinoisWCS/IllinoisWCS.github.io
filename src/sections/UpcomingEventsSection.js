9833f6a692bf76297a5eb67e398a5d7bbb349601
kikis05 <kirthis2@illinois.edu>
2026-02-11 17:25:42 -0600

    remove students only

--- src/sections/UpcomingEventsSection.js
+++ src/sections/UpcomingEventsSection.js
@@ -153,7 +153,6 @@ export default function UpcomingEventsSection() {
               className={styles.openOffice}
             >
               <h3 style={{ margin: '0' }}>Open Office</h3>
-              <p> (students only)</p>
               <div className={styles.dateTime}>
                 <p>Mon-Fri</p>
                 <p>2:00 PM - 5:00 PM</p>
